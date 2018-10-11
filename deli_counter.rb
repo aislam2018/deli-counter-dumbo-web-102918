84a0d6a97527c6306a8d5a600f4a2c1cf61a8b44
anik1606 <anik1606@gmail.com>
2018-10-11 01:20:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def take_a_number(katz_deli, name)
   return "Welcome, #{name}. You are number #{num} in line."
 end
 def line(katz_deli)
+  if katz_deli == nil 
+    return ""
   arr = []
   counter = 1
   katz_deli.each do |name|
@@ -13,4 +15,7 @@ def line(katz_deli)
   end
   return "The line is currently:" + arr.join
 end
-def now_serving
+def now_serving(katz_deli)
+  name = katz_deli.shift
+  return "Currently serving #{name}."
+end
