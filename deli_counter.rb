b8bf02e076ef9608325aa9a3b2765e256da9daf6
anik1606 <anik1606@gmail.com>
2018-10-11 01:08:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,4 +4,6 @@ def take_a_number(katz_deli, name)
   return "Welcome, #{name}. You are number #{num} in line."
 end
 def line(katz_deli)
-  
+  counter = 1
+  katz_deli.each do |name|
+    counter += 1 
